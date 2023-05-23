3117e4fd1c549153e46ce190b9656ec7b584ba87
stormrage-web <sharfull33@gmail.com>
2023-05-23 22:55:19 +0300

    chore: refactored date and styles

---
+++ src/shared/mocks/flights.ts
@@ -0,0 +1,28 @@
+interface Flight {
+	id: number;
+	time: string;
+	title: string;
+}
+
+export const flights: Flight[] = [
+	{
+		id: 0,
+		title: "SU 0019",
+		time: "11:11",
+	},
+	{
+		id: 1,
+		title: "SU 0019",
+		time: "11:11",
+	},
+	{
+		id: 2,
+		title: "SU 0019",
+		time: "11:11",
+	},
+	{
+		id: 3,
+		title: "SU 0019",
+		time: "11:11",
+	},
+];
